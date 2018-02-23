00fd27e704528f71260799a043f41085253f6e46
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-02-23 14:18:34 +0300

    Mk0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    try {
+         if(currency>10000) throw {error:'You are rich, my friend! We don\'t have so much coins for exchange'};
+         if(currency<=0) throw {};
+    } catch (err) {
+        return err;
+    }
+    let result={};
+    let settings={'H':50, "Q":25, "D":10, "N":5, "P":1 };
+    for (var key in settings) {
+    if(currency>=settings[key]){
+        result[key]=0;
+        while(currency>=settings[key]){
+            result[key]++;
+            currency-=settings[key];
+        }
+    }
+    }
+    return result;
 }
